e5c6f06b50a5e2cbcf53472e570603aa3ac4928a
TAKBEDILNDASMU <titanaprilian73@gmail.com>
2025-06-13 08:59:32 +0700

    fix password minimum validation

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -19,7 +19,7 @@ const formSchema = z
   .object({
     username: z.string().min(3, { message: "Username must be at least 3 characters." }),
     email: z.string().email({ message: "Please enter a valid email address." }),
-    password: z.string().min(6, { message: "Password must be at least 8 characters." }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
